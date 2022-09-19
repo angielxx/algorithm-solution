f6b3aff74b17c8e72b004af013edf7adabb7e49d
angielxx <94applekoo@naver.com>
2022-09-19 23:19:35 +0900

    solve problems at 220919

---
+++ 파알인/문제80.태스크-스케줄러/s2.py
@@ -0,0 +1,25 @@
+import collections
+
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        counter = collections.Counter(tasks)
+        result = 0
+
+        while True:
+            sub_count = 0
+
+            # 개수 순 추출
+            for task, _ in counter.most_common(n+1):
+                sub_count += 1
+                result += 1
+
+                counter.subtract(task)
+                # 0 이하인 아이템을 목록에서 완전히 제거
+                counter += collections.Counter()
+
+            if not counter:
+                break
+
+            result += n - sub_count + 1
+
+        return result
