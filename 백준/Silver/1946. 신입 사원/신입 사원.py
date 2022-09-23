b9e7eb3a759e942564750c578e037f39f463837b
이은지 / Angie Lee <103434451+angielxx@users.noreply.github.com>
2022-09-23 13:32:52 +0900

    [Silver I] Title: 신입 사원, Time: 5520 ms, Memory: 70984 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    score = [list(map(int, input().split())) for _ in range(N)]
+    score.sort()
+
+    cnt = 1
+    # 1등의 사람의 면접 순위
+    top = score[0][1]
+    for i in range(1, N):
+        if score[i][1] < top:
+            cnt += 1
+            top = score[i][1]
+        else:
+            pass
+    print(cnt)
