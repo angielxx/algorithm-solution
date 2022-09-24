a25fceebaf88ba371b15f5ecfccb39ad1d429df0
이은지 / Angie Lee <103434451+angielxx@users.noreply.github.com>
2022-09-24 23:09:06 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -5,59 +5,34 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = list(map(int, input().split()))
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))
 
-# 0 1 2 3
-# +, -, *, /
-opt_li = list(map(int, input().split()))
-opt = [0]*(opt_li[0]) + [1] * (opt_li[1]) + [2] * (opt_li[2]) + [3] * (opt_li[3])
-# print('opt', opt)
+maximum = float('-inf')
+minimum = float('inf')
 
-max_result = float('-inf')
-min_result = float('inf')
+def dfs(depth, total, plus, minus, mult, div):
+    global maximum
+    global minimum
 
-def dfs(idx, subset=[]):
-    global max_result
-    global min_result
-
-    if len(subset) == N-1:
-        # print('subset', subset)
-        # 계산하기
-        temp = arr[:]
-        num1 = temp.pop(0)
-        # print('temp', temp)
-        for i in range(len(subset)):
-            idx = subset[i]
-            # print('num1',num1)
-            # print(temp[i])
-            if opt[idx] == 0:
-                num1 += temp[i]            
-            elif opt[idx] == 1:
-                num1 -= temp[i]            
-            elif opt[idx] == 2:
-                num1 *= temp[i]            
-            elif opt[idx] == 3:
-                if num1 < 0 and temp[i] > 0:
-                    num1 = -((-(num1)) // temp[i])
-                else:
-                    num1 = num1 // temp[i]
-        # print(num1)
-        if num1 > max_result:
-            max_result = num1
-        if num1 < min_result:
-            min_result = num1
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
         return
-    
-    # print(subset)
-    # dfs 계속
-    for i in range(N-1):
-        if i in subset:
-            continue
-        dfs(i, subset + [i])
 
-# dfs 시작
-for i in range(N-1):
-    dfs(i, [i])
+    if plus:
+        dfs(depth+1, total + num[depth], plus-1, minus, mult, div)
+    if minus:
+        dfs(depth+1, total - num[depth], plus, minus-1, mult, div)
+    if mult:
+        dfs(depth+1, total * num[depth], plus, minus, mult-1, div)
+    if div:
+        if total < 0 and num[depth] > 0:
+            total = -((-(total)) // num[depth])
+        else:
+            total = total // num[depth]
+        dfs(depth+1, total, plus, minus, mult, div-1)
 
-print(max_result)
-print(min_result)
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
