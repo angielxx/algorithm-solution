930c224d5dcf2800d06f99171b0eb37d43f14b09
이은지 / Angie Lee <103434451+angielxx@users.noreply.github.com>
2022-09-24 21:00:13 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 220 ms, Memory: 62,224 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
@@ -1,59 +1,63 @@
 T = int(input())
 
-def oslo(i, j, color):
-    me = 'B' if color == 1 else 'W'
+# 좌상부터 시계방향 0 ~ 7 에 따라 행, 열의 값 변화
+dir = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
+
+# T, X 방향으로 바꿀 수 있는 돌이 있는지 확인하는 함수
+# si, sj : 현재 놓은 돌의 위치, color : 나의 색깔
+def change(sj, si, color):
+    global arr
+
     enemy = 'W' if color == 1 else 'B'
-    arr[i][j] = me
- 
-    # 왼쪽 위 대각선방향부터 시계방향 델타탐색
-    delta = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
+    me = 'B' if color == 1 else 'W'
 
     for k in range(8):
-        si, sj = i, j
-        # 바꿀 적 돌의 위치
-        toChange = []
+        # 방향 k에 대해 계속 상대 돌이 나오는지 확인
+        i = 1
+        status = False
+        stack = []
         while True:
-            ni, nj = si+delta[k][0], sj+delta[k][1]
-            if 0 <= ni < N and 0 <= nj < N:
-                # 0 나오면 바로 중단
-                if arr[ni][nj] == 0:
-                    break
-                # 적이 나오면 바꿀 적의 돌 추가
-                if arr[ni][nj] == enemy:
-                    toChange.append((ni, nj))
-                # 나 자신이 나오면 저장해놨던 바꿀 적의 돌 바꾸기
-                elif arr[ni][nj] == me:
-                    # toChange가 없으면 나 자신 다음에 바로 나 자신이 나온 것
-                    if toChange:
-                        for ci, cj in toChange:
-                            arr[ci][cj] = me
-                    # 나 자신이 한번 나온 곳 까지만 바꿀 수 있기 때문에 바꾸고 나면 바로 그 방향에 대한 탐색 중단
-                    break
+            ni, nj = si + dir[k][0] * i, sj + dir[k][1] * i
+            if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == enemy:
+                stack.append((ni, nj))
+                i += 1
+            # 연속해서 enemy 나오다가 자기자신이면
+            elif 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == me:
+                status = True
+                break
+            # 0이 나와도 중단
             else:
                 break
-            si, sj = ni, nj
+        # print(stack, k, status)
+        while stack and status:
+            ki, kj = stack.pop()
+            arr[ki][kj] = me
+            # print('change', arr)
 
-for tc in range(1,T+1):
+for tc in range(1, T+1):
     N, M = map(int, input().split())
-
     arr = [[0]*N for _ in range(N)]
-    # 초기 흑돌, 백돌 놓기
-    arr[N//2-1][N//2-1] = arr[N//2][N//2] = 'W'
-    arr[N//2][N//2-1] = arr[N//2-1][N//2] = 'B'
 
+    # 초기 흑돌 백돌 배치
+    arr[N//2 - 1][N//2 - 1] = 'W'
+    arr[N//2][N//2] = 'W'
+    arr[N//2 - 1][N//2] = 'B'
+    arr[N//2][N//2 - 1] = 'B'
+
+    # (j, i, 1:흑돌 / 2:백돌)
     for _ in range(M):
         j, i, color = map(int, input().split())
-        i -= 1
-        j -= 1
-        oslo(i, j, color)
-
-    # 최종 흑돌, 백돌 수 세기
-    B_cnt = W_cnt = 0
+        # 번호를 -1하여 인덱스로 변환
+        j, i = j-1, i-1
+        arr[i][j] = 'B' if color == 1 else 'W'
+        # print(arr)
+        change(j, i, color)
+    # 개수 세기
+    W_cnt = B_cnt = 0
     for i in range(N):
         for j in range(N):
-            if arr[i][j] == 'B':
-                B_cnt += 1
-            elif arr[i][j] == 'W':
+            if arr[i][j] == 'W':
                 W_cnt += 1
-
+            elif arr[i][j] == 'B':
+                B_cnt += 1
     print('#{}' .format(tc), B_cnt, W_cnt)
