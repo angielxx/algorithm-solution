a67e24a798b97a412164f3fd6a4b8a0e54299e01
이은지 / Angie Lee <103434451+angielxx@users.noreply.github.com>
2022-09-25 16:53:49 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 126 ms, Memory: 60,104 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -0,0 +1,21 @@
+T = 10
+
+for _ in range(1, T+1):
+    tc = int(input())
+    nums = list(map(int, input().split()))
+
+    i = 1
+    while True:
+        # print(i)
+        # 맨 앞의 원소 삭제
+        num = nums.pop(0) - i
+        # 맨 뒤에 추가
+        nums.append(num)
+        if num <= 0:
+            nums[-1] = 0
+            break
+        i = (i + 1) % 5
+        if i == 0:
+            i = 5
+
+    print('#{}' .format(tc), *nums)
