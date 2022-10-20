28e52a72529bc28660f2dde31b62e4dc8807acd4
이은지 / Angie Lee <angielxx94@gmail.com>
2022-10-21 02:03:11 +0900

    Time: 567 ms (5.01%), Space: 55.6 MB (5.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,20 +5,13 @@
  */
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        console.log('nums', nums);
-        console.log('i', i)
         const arr = [...nums]
         arr.splice(i, 1);
         const diff = target - nums[i];
-        console.log('arr', arr);
-        console.log('diff', diff);
         if (arr.includes(diff)) {
-            console.log('i', i)
             const idx = (function () {
                 for (let j = 0; j < nums.length; j++) {
-                    console.log('j', j)
                     if ((nums[j] === diff) && (j != i)) {
-                        console.log('j')
                         return j;
                     }
                 }
