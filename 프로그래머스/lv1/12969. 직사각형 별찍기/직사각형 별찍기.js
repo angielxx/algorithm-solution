96aa1b3b736822b848e9d1e3484a62359ca5249c
이은지 / Angie Lee <angielxx94@gmail.com>
2022-12-05 16:47:51 +0900

    [level 1] Title: 직사각형 별찍기, Time: 44.11 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,8 +2,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    let line = [];
-    for (let i = 0; i < a; i++) line.push('*');
-    line = line.join('')
-    for (let j = 0; j < b; j++) console.log(line)
+    const line = '*'.repeat(a)
+    for (let i = 0; i < b; i++) console.log(line)
 });
