16e5c05db63ecf9b0619bd9c2389390f6647b112
이은지 / Angie Lee <angielxx94@gmail.com>
2022-12-08 17:38:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 256 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -5,40 +5,41 @@ let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 const N = Number(input[0])
 const arr = input.slice(1).map(Number)
 
-// function bubbleSort(arr) {
-//   // i : 교환하는 범위의 맨 마지막 원소의 인덱스
-//   for (let i = arr.length; i > 0; i--) {
-//     // j : 교환하는 범위의 첫번째 원소의 인덱스
-//     for (let j = 0; j < i - 1; j++) {
-//       if (arr[j] > arr[j + 1]) {
-//         let temp = arr[j]
-//         arr[j] = arr[j + 1]
-//         arr[j + 1] = temp
-//       }
-//     }
-//   }
-//   return arr
-// }
-
-// bubbleSort(arr)
-
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let MIN = i
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[MIN]) {
-        MIN = j
+function bubbleSort(arr) {
+  // i : 교환하는 범위의 맨 마지막 원소의 인덱스
+  for (let i = arr.length; i > 0; i--) {
+    // j : 교환하는 범위의 첫번째 원소의 인덱스
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
       }
-    }
-    if (i !== MIN) {
-      let temp = arr[i]
-      arr[i] = arr[MIN]
-      arr[MIN] = temp
     }
   }
   return arr
 }
-selectionSort(arr)
+
+bubbleSort(arr)
+
+// function selectionSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let MIN = i
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[MIN]) {
+//         MIN = j
+//       }
+//     }
+//     if (i !== MIN) {
+//       let temp = arr[i]
+//       arr[i] = arr[MIN]
+//       arr[MIN] = temp
+//     }
+//   }
+//   return arr
+// }
+// selectionSort(arr)
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i])
 }
+// arr.forEach((el) => console.log(el)) // 더 오래걸림
