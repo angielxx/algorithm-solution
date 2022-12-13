2632c3bc0a0abf1bbaaacb28e649000efe6bba5d
이은지 / Angie Lee <angielxx94@gmail.com>
2022-12-13 17:28:25 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,11 +1,10 @@
 function solution(strings, n) {
-  strings.sort((a, b) => {
-    const gap = a.charCodeAt(n) - b.charCodeAt(n)
-    if (gap === 0) {
-      if (a > b) return 1
-      else if (a < b) return -1
+    strings.sort((a, b) => {
+    if (a[n] === b[n]) {
+        if (a > b) return 1
+        else if (a < b) return -1
     } else {
-      return gap
+      return a.charCodeAt(n) - b.charCodeAt(n)
     }
   })
   return strings
