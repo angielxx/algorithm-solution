ced09a333141a57815e703edf99d35e06cb0c6ab
이은지 / Angie Lee <angielxx94@gmail.com>
2022-12-15 14:00:21 +0900

    [level 1] Title: 실패율, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,16 +1,14 @@
 function solution(N, stages) {
-  let counter = Array.from({ length: N + 2 }, (_) => 0)
   let rate = {}
 
-  for (let i = 0; i < stages.length; i++) {
-    counter[stages[i]]++
-  }
   for (let stage = 1; stage <= N; stage++) {
-    fail = counter[stage] / counter.slice(stage).reduce((a, b) => a + b)
+    fail =
+      stages.filter((x) => x === stage).length /
+      stages.filter((x) => x >= stage).length
     rate[stage] = fail
   }
-  let answer = Object.keys(rate)
+
+  return Object.keys(rate)
     .map(Number)
     .sort((a, b) => (rate[a] === rate[b] ? a - b : rate[b] - rate[a]))
-return answer
 }
