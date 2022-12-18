06fe149da3b70fa81143455ad63604bc62afc963
이은지 / Angie Lee <angielxx94@gmail.com>
2022-12-18 18:38:07 +0900

    [level 2] Title: 피보나치 수, Time: 1.59 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,16 +1,11 @@
 function solution(n) {
-  let arr = Array.from({ length: 100000 }, (_) => 0)
-  arr[0] = 0
-  arr[1] = 1
-  //   console.log(arr.slice(0, 6))
-  function fib(n) {
-    if (n <= 1) return arr[n]
-    if (n >= 2) {
-      for (let i = 2; i <= n; i++) {
-        arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567
-      }
-      return arr[n]
-    }
+  let a = 0
+  let b = 1
+  let f = 1
+  for (let i = 2; i <= n; i++) {
+    f = (a + b) % 1234567
+    a = b
+    b = f
   }
-  return fib(n)
+  return f
 }
