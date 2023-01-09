d500aace55b10bcf7f16ea620fb89284419b956f
이은지 / Angie Lee <angielxx94@gmail.com>
2023-01-09 20:33:59 +0900

    [level 2] Title: 주식가격, Time: 84.84 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -2,10 +2,8 @@ def solution(prices):
     answer = [0 for _ in range(len(prices))]
 
     for i in range(len(prices) - 1):
-        cnt = 0
         for j in range(i+1, len(prices)):
-            cnt += 1
+            answer[i] += 1
             if (prices[j] < prices[i]):
               break
-        answer[i] = cnt   
     return answer
