15e0a9167f19fe3c6d824b01d732a8b671e3c88b
이은지 <angielxx94@gmail.com>
2023-02-22 21:58:18 +0900

    [level 3] Title: 최고의 집합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.js
@@ -1,13 +1,16 @@
 function solution(n, s) {
-  const share = s / n >> 0;
-  
-  if(!share) return [-1];
-  
+  // 시작 최댓값
+  const share = (s / n) >> 0;
+
+  // 베분할 수 없는 경우 (나머지를 쪼개 share에 배분해줄 값이 없다)
+  if (!share) return [-1];
+
+  // 나머지
   const rest = s % n;
+
   const answer = new Array(n).fill(share);
-  
-  for(let i = 0; i < rest; i ++)
+  for (let i = 0; i < rest; i++) {
     answer[answer.length - 1 - i]++;
-  
+  }
   return answer;
 }
