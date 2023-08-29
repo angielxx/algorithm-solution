d110538c8172d65b1e0339eb47ff484095c676b0
이은지 <angielxx94@gmail.com>
2023-08-29 14:42:29 +0900

    [Gold IV] Title: 연구소, Time: 632 ms, Memory: 127672 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -5,52 +5,61 @@ input = sys.stdin.readline
 
 # 세로, 가로
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
 
 virus = []
+empty = []
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 2:
+        if graph[i][j] == 0:
+            empty.append((i, j))
+        elif graph[i][j] == 2:
             virus.append((i, j))
+
+def chooseWall(arr, cnt):
+    result = []
+    if cnt == 0:
+        return [[]]
+
+    for i in range(len(arr)):
+        el = arr[i]
+        rest  = chooseWall(arr[i + 1:], cnt - 1)
+        for r in rest:
+            result.append([el] + r)
+    return result
 
 answer = 0
 
-def bfs(graph):
+def bfs(wall):
     global answer
     
-    di = [-1, 1, 0, 0]
-    dj = [0, 0, -1, 1]
+    arr = deepcopy(graph)
+    for i, j in wall:
+        arr[i][j] = 1
     
     q = deque(virus)
+    
     while q:
-        i, j = q.popleft()
+        si, sj = q.popleft()
         for k in range(4):
-            ni = i + di[k]
-            nj = j + dj[k]
-            if ni < 0 or ni >= n or nj < 0 or nj >= m or graph[ni][nj] != 0:
+            ni = si + di[k]
+            nj = sj + dj[k]
+            if ni < 0 or ni >= n or nj < 0 or nj >= m or arr[ni][nj] != 0:
                 continue
-            graph[ni][nj] = 2
+            arr[ni][nj] = 2
             q.append((ni, nj))
-            
-    cnt  = 0
+    cnt = 0
     for i in range(n):
         for j in range(m):
-            if graph[i][j] == 0:
+            if arr[i][j] == 0:
                 cnt += 1
     if cnt > answer:
         answer = cnt
 
-def makeWall(cnt, graph):
-    if cnt == 3:
-        bfs(graph)
-        return
-    
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 0:
-                graph[i][j] = 1
-                makeWall(cnt + 1, deepcopy(graph))
-                graph[i][j] = 0
+for wall in chooseWall(empty, 3):
+    bfs(wall)
 
-makeWall(0, arr)
 print(answer)
